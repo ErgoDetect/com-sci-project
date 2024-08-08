24e8de9fe1ddd8d8d15ba95ab1facecca8dbedf1
Sealed <korn2400@gmail.com>
2024-08-08 22:04:37 +0700

    Revert "Merge pull request #29 from ErgoDetect/korn"
    This reverts commit 1f48de2bf463d77ae62fc45425cb4ee1cd1120c2, reversing
    changes made to 0bd090f384dc9a8349dbeee46c9d218c3c07da4e.

--- src/components/videoFeed.tsx
+++ src/components/videoFeed.tsx
@@ -8,17 +8,12 @@ import React, {
   useRef,
 } from 'react';
 import { Button } from 'antd';
-import {
-  DeviceProps,
-  videoFeedProps,
-  LandmarksResult,
-} from '../interface/propsType';
+import { DeviceProps, videoFeedProps } from '../interface/propsType';
 import DeviceSelector from './camera/deviceSelector';
 import WebcamDisplay from './camera/webcamDisplay';
 import useWebSocket from '../utility/webSocketConfig';
 import { useResData } from '../context';
 
-import filterLandmark from '../utility/filterLandmark';
 // Define a type for the throttle function
 type ThrottleFunction = (...args: unknown[]) => void;
 
@@ -105,13 +100,8 @@ const VideoFeed: React.FC<videoFeedProps> = ({ width, borderRadius }) => {
   const sendLandMarkData = useCallback(() => {
     const currentTime = Date.now();
     if (currentTime - lastLogTimeRef.current >= logInterval) {
-      send(
-        JSON.stringify({
-          data: filterLandmark(landMarkData as LandmarksResult),
-          timestamp: currentTime,
-        }),
-      );
-      // send(JSON.stringify({ landMarkData, timestamp: currentTime }));
+      console.log(landMarkData);
+      send(JSON.stringify({ landMarkData, timestamp: currentTime }));
       lastLogTimeRef.current = currentTime;
     }
   }, [landMarkData, logInterval, send]);
@@ -129,8 +119,8 @@ const VideoFeed: React.FC<videoFeedProps> = ({ width, borderRadius }) => {
     };
   }, [throttle, sendLandMarkData, logInterval]);
 
-  // console.log('face landmark', landMarkData?.faceResults);
-  // console.log('pose landmark', landMarkData?.poseResults);
+  console.log('face landmark', landMarkData?.faceResults);
+  console.log('pose landmark', landMarkData?.poseResults);
 
   return (
     <>
