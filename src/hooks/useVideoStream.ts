31e80084017c77f9aa56a3f2ff87d90c875ed566
Sealed <korn2400@gmail.com>
2024-08-11 00:13:35 +0700

    Update useVideoStream.ts

--- src/hooks/useVideoStream.ts
+++ src/hooks/useVideoStream.ts
@@ -44,8 +44,8 @@ const useVideoStream = ({
 
     const constraints = {
       video: {
-        width: { ideal: 1920 },
-        height: { ideal: 1080 },
+        width: { ideal: 1280 },
+        height: { ideal: 720 },
         frameRate: { ideal: 30 },
       },
       audio: false,
