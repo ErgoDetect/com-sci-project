054c6c5f881b914f3cdfde1e71cc98417990e400
Amsomeboys <khnun1090@gmail.com>
2024-10-22 02:36:35 +0700

    config usage model

--- src/model/faceLandmark.tsx
+++ src/model/faceLandmark.tsx
@@ -21,9 +21,8 @@ export const initializeFaceLandmarker = async () => {
       {
         baseOptions: {
           modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
-          delegate: undefined,
+          delegate: 'GPU',
         },
-        outputFaceBlendshapes: true,
         runningMode: 'VIDEO',
         numFaces: 1,
       },
